7b0115fb7b45f1b8cf73876942721f256fb2f899
JonathanQ-Maker <jonathanqfxw@gmail.com>
2023-06-25 00:18:03 -0700

    implemented proper setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import io
+import os
 
-if __name__ == "__main__":
-    setup()
+
+with io.open(os.path.join(".", 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name="reflect",
+    version="0.0.1",
+    description="Numpy based machine learning library",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="JonathanQ-Maker",
+    author_email="jonathanqfxw@gmail.com",
+    url="https://github.com/JonathanQ-Maker/reflect",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=["numpy"],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
